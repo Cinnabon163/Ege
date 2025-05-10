618085e1d0d27f5b4558fe44a6b3a94165c090fd
Егор Кураговский <uniformplay6@gmail.com>
2025-05-10 22:38:38 +0300

    17.6 fix

--- 17/6/17.py
+++ 17/6/17.py
@@ -11,12 +11,19 @@ for str_num in content:
 
 max36 = max(multiples_of_36)
 
+assert max36 % 36 == 0
+
 def is_positive(x):
     x = int(x)
     return x > 0
 
 def endswith_36(x):
     return str(x)[-2:] == "36"
+
+assert endswith_36("1236") == True
+assert endswith_36("-1236") == True
+assert endswith_36("23343") == False
+
 
 # не менее чем для двух элементов выполняется хотя бы одно
 # из условий (или оба): элемент положительный, заканчивается на 36.
@@ -29,14 +36,19 @@ def get_triple_status(current, prev, prev_prev, max36):
         if is_positive(x) or endswith_36(x):
             count += 1
         total += int(x)
-    return (count > 2 and total < max36, total)
+    return (count >= 2 and total < max36, total)
 
+triple_good = (str(35), str(-36), str(36))
+assert get_triple_status(*triple_good, max36=max36) == (True, 35-36+36)
 
+triple_bad = (str(35), str(-36), str(-39))
+assert get_triple_status(*triple_bad, max36=max36) == (False, 35-36-39)
 
 count = 0
 totals = []
 for i, _ in enumerate(content[2:], 2):
-    status = get_triple_status(content[i], content[i-1], content[i-2], max36)
+    triple = (content[i], content[i-1], content[i-2])
+    status = get_triple_status(*triple, max36)
     is_triple_valid = status[0]
     totals.append(status[1])
     if is_triple_valid:
