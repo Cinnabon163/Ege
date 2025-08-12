eee2d05a1602bb5f160e36d1317aa44288de9db1
Егор Кураговский <uniformplay6@gmail.com>
2025-08-12 19:10:37 +0300

    var2

--- 15/task_2.py
+++ 15/task_2.py
@@ -1,1 +1,13 @@
-def f(x, A):
+def f(x):
+    l1 = x % 30 == 0
+    l2 = 15 % x == 0 
+    return (l1 or l2)
+
+for x in range(2, 100):
+    if f(x):
+        print(x, end=',')
+        
+
+
+
+        
